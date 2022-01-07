7c2075be0dc772f76f70173034f912413f6df547
Hana Stupková <hstupkova@gmail.com>
2022-01-07 10:36:21 +0100

    this keyword

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 class Department {
-  // name: string = 'DEFAULT';
   name: string;
 
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: 'Accounting2', describe: accounting.describe };
+accountingCopy.describe();
