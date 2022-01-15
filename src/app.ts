4aebe04a9bc3dcd33ca3115051d84be3a59874d7
Hana Stupková <hstupkova@gmail.com>
2022-01-15 16:28:22 +0100

    overriding properties, protected modifier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -36,6 +36,13 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string): void {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -45,18 +52,13 @@ class AccountingDepartment extends Department {
   }
 }
 
-/* const accounting = new Department('d1', 'Accounting');
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-
-// accounting.employees[2] = 'Anna';
-
-accounting.describe();
-accounting.printEmployeeInformation(); */
-
 const it = new ITDepartment('d2', ['Max']);
 console.log(it);
 
 const accounting = new AccountingDepartment('d1', []);
 accounting.addReport('Something went wrong...');
 accounting.printReports();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
