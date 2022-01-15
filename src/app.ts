da78d8a4660a2529872bc3064fc04ef1891e3568
Hana Stupková <hstupkova@gmail.com>
2022-01-15 16:47:25 +0100

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -32,8 +32,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value.');
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
   }
 
   addEmployee(name: string): void {
@@ -45,6 +62,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -56,9 +74,15 @@ const it = new ITDepartment('d2', ['Max']);
 console.log(it);
 
 const accounting = new AccountingDepartment('d1', []);
+
 accounting.addReport('Something went wrong...');
 accounting.printReports();
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 accounting.printEmployeeInformation();
+
+console.log(accounting.mostRecentReport);
+
+accounting.mostRecentReport = 'Year 2021 report';
+console.log(accounting.mostRecentReport);
